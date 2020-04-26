840bcf8b4ffceadd7527e03c2823c147465c4300
Aleksei Toropov <45897514+thetoropov@users.noreply.github.com>
2020-04-26 23:46:51 +0300

    add pz7

---
+++ pz7/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace pz7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(615, 40);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.AutoSize = true;
+            this.button1.Location = new System.Drawing.Point(262, 190);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.button1_MouseMove);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(615, 438);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResizeEnd += new System.EventHandler(this.RunningForm_ResizeEnd);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ pz7/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pz7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Point pos; //start position
+        Point delta; //speed
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            ChangePositionButton();
+        }
+
+        private void ChangePositionButton()
+        {
+            Point newPos;
+            newPos = MousePosition;
+            CalcDelta(newPos);
+
+            if(button1.Location.X - delta.X < 5)
+            {
+                button1.Location = new Point(Form1.ActiveForm.Width - button1.Width - 20, button1.Location.Y);
+            }
+            else if (button1.Location.X - delta.X > Form1.ActiveForm.Width - button1.Width - 20)
+            {
+                button1.Location = new Point(5, button1.Location.Y);
+            }
+            else if (button1.Location.Y - delta.Y < 5)
+            {
+                button1.Location = new Point(button1.Location.X, Form1.ActiveForm.Height - button1.Height - 35);
+            }
+            else if (button1.Location.Y - delta.Y > Form1.ActiveForm.Height - button1.Height - 35)
+            {
+                button1.Location = new Point(button1.Location.X, 5);
+            }
+            else
+            {
+                button1.Location = new Point(button1.Location.X - delta.X, button1.Location.Y - delta.Y);
+            }
+        }
+        
+        private void CalcDelta(Point newPos)
+        {
+            delta.X = pos.X - newPos.X;
+            delta.Y = pos.Y - newPos.Y;
+            pos = newPos;
+        }
+
+
+        private void RunningForm_ResizeEnd(object sender, EventArgs e)
+        {
+            if (button1.Location.X < 5)
+            {
+                button1.Location = new Point(5, button1.Location.Y);
+            }
+
+            if (button1.Location.X > Form1.ActiveForm.Width - button1.Width - 20)
+            {
+                button1.Location = new Point(Form1.ActiveForm.Width - button1.Width - 20, button1.Location.Y);
+            }
+
+            if (button1.Location.Y < 5)
+            {
+                button1.Location = new Point(button1.Location.X, 5);
+            }
+
+            if (button1.Location.Y > Form1.ActiveForm.Height - button1.Height - 35)
+            {
+                button1.Location = new Point(button1.Location.X, Form1.ActiveForm.Height - button1.Height - 35);
+            }
+        }
+
+        private void button1_MouseMove(object sender, MouseEventArgs e)
+        {
+            ChangePositionButton();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Поздравляем! Вы смогли нажать на кнопку!");
+            Close();
+        }
+    }
+}
