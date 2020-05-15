180ca212c9bea7d4eeffe97195c69d35bf40d576
Aleksei Toropov <45897514+thetoropov@users.noreply.github.com>
2020-05-15 10:16:36 +0300

    upd server

---
+++ Tcp.Server/EnteringPointServer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using SomeProject.Library.Server;
+
+namespace SomeProject.TcpServer
+{
+    class EnteringPointServer
+    {
+        static void Main(string[] args)
+        {
+           try
+            {
+                Server server = new Server();
+                server.TurnOnListener().Wait();
+                
+                //server.turnOffListener();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
